aa7ae1e35e925c5fb541715ea5de89c21e67613b
ahmadfarid <sabbahahmad093@gmail.com>
2021-07-27 16:58:00 +0300

    start fetching price API Relates #5'

---
+++ scripts/CurrencyApi.js
@@ -0,0 +1,9 @@
+let sympol;
+let currUrl = `https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT`
+const sympols = ['BTCUSDT','ETHUSDT','ETCUSDT','DOGEUSDT','NANOUSDT','ONEUSDT','OMGUSDT','BATUSDT','DNTUSDT'];
+const setUrl = () => {
+    for (let i=0; i<sympols.length; i++){
+
+    }
+
+}
