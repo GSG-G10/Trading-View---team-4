33bbee9659d36a35f18efa8ab69b1490afc7af48
ahmadfarid <sabbahahmad093@gmail.com>
2021-07-27 20:58:28 +0300

    complete the function that creates the price div and fill it with information from API object Relates #5

--- scripts/CurrencyApi.js
+++ scripts/CurrencyApi.js
@@ -1,16 +1,38 @@
-let sympol;
-let currUrl = `https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT`
-const sympols = ['BTCUSDT','ETHUSDT','ETCUSDT','DOGEUSDT','NANOUSDT','ONEUSDT','OMGUSDT','BATUSDT','DNTUSDT'];
-const tes = (obj)=>{
-    
-    console.log('xhr.responseText')
-}
+// let sympol;
+let priceUrl = `https://api.binance.com/api/v3/ticker/price?symbol=`;
+const sympols = [
+  "BTCUSDT",
+  "ETHUSDT",
+  "ETCUSDT",
+  "DOGEUSDT",
+  "NANOUSDT",
+  "ONEUSDT",
+  "OMGUSDT",
+  "BATUSDT",
+  "DNTUSDT",
+];
 const setUrl = () => {
-    for (let i=0; i<sympols.length; i++){
-        currUrl = `https://api.binance.com/api/v3/ticker/24hr?symbol=${sympols[i]}`
-        fetchFromURL(currUrl, tes)
-       
-    }
-}
-setUrl()
-
+  for (let i = 0; i < sympols.length; i++) {
+    priceUrl = `https://api.binance.com/api/v3/ticker/price?symbol=${sympols[i]}`;
+    fetchFromURL(priceUrl, (obj) => {
+      const object = JSON.parse(obj);
+      const name = sympols[i];
+      const price = object.price;
+      appendElement(sympolsContainer, sympolDiv);
+      const sympolName = createElement("h2");
+      addClass(sympolDiv, "sympol-name");
+      sympolName.textContent = name;
+      appendElement(sympolDiv, sympolName);
+      const priceDiv = createElement("div");
+      addClass(priceDiv, "last-price");
+      appendElement(sympolDiv, priceDiv);
+      const pricePara = createElement("p");
+      pricePara.textContent = "price: ";
+      appendElement(priceDiv, pricePara);
+      const priceNum = createElement("p");
+      priceNum.textContent = price;
+      appendElement(priceDiv, priceNum);
+    });
+  }
+};
+setUrl();
