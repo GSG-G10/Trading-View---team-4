323a03ac4bdafbf2539ccdf789b9731c0476d480
ahmadfarid <sabbahahmad093@gmail.com>
2021-07-27 20:59:02 +0300

    Prettier format

--- scripts/DomFunctions.js
+++ scripts/DomFunctions.js
@@ -2,12 +2,12 @@
 const sympolsContainer = document.querySelector(".sympols-section");
 const sympolDiv = document.querySelector(".sympol-container");
 /* ************************** Generic functions for Dom ************************ */
-const appendElement = (parent,child)=>{
-    parent.appendChild(child)
-}
+const appendElement = (parent, child) => {
+  parent.appendChild(child);
+};
 const createElement = (tag) => {
-    return document.createElement(tag)
-}
-const addClass = (ele,className) => {
-    ele.classList.add(className)
-}
+  return document.createElement(tag);
+};
+const addClass = (ele, className) => {
+  ele.classList.add(className);
+};
