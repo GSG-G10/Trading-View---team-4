90fb3c4e38d44e94a21b55b365a55cd88f5caf61
ahmadfarid <sabbahahmad093@gmail.com>
2021-07-28 13:46:16 +0300

    delete fetching price API and work on one API Relates #24

--- scripts/CurrencyApi.js
+++ scripts/CurrencyApi.js
@@ -14,18 +14,17 @@ const sympols = [
   "BATUSDT",
   "DNTUSDT",
 ];
-const setUrl = () => {
+// function to fetch price data from API ---------------------
+const fetchPriceData = () => {
   for (let i = 0; i < sympols.length; i++) {
-    let priceUrl = `https://api.binance.com/api/v3/ticker/price?symbol=${sympols[i]}`;
+    let priceUrl = `https://api.binance.com/api/v3/ticker/24hr?symbol=${sympols[i]}`;
 
     fetchFromURL(priceUrl, (obj) => {
       createCard(i, obj);
     });
-    let changesUrl = `https://api.binance.com/api/v3/ticker/24hr?symbol=${sympols[i]}`;
-    fetchFromURL(changesUrl, addPriceChanges);
   }
 };
-setUrl();
+fetchPriceData(); // invoke the function
 searchBtn.addEventListener('click',() => {
   clearCards();
   let priceUrl = `https://api.binance.com/api/v3/ticker/price?symbol=${searchInput.value}`;
