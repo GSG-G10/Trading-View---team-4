3e485d8fde313dc3bbb73c2595ad3efb32f85962
M7Mansour <mdmansour2000@gmail.com>
2021-07-28 18:47:32 +0300

    wrote testing functions

---
+++ scripts/Test.js
@@ -0,0 +1,31 @@
+describe('', () => {
+    test('', () => {
+        const actual;
+        const expected;
+        expect(actual).toBe(expected);
+    });
+
+    test('', () => {
+        const actual;
+        const expected;
+        expect(actual).toBe(expected);
+    });
+
+    test('', () => {
+        const actual;
+        const expected;
+        expect(actual).toBe(expected);
+    });
+
+    test('', () => {
+        const actual;
+        const expected;
+        expect(actual).toBe(expected);
+    });
+
+    test('', () => {
+        const actual;
+        const expected;
+        expect(actual).toBe(expected);
+    });
+});
