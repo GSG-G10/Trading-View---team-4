9c878f86900bcb2a81e06db34acf526b1ba4480b
muhammadharoun <hro19502001@gmail.com>
2021-07-29 09:55:50 +0300

    edit general.js

--- scripts/General.js
+++ scripts/General.js
@@ -1,11 +1,39 @@
 const fetchFromURL = (url, cb) => {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            cb(JSON.parse(xhr.responseText));
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                cb(JSON.parse(xhr.responseText));
+            } else {
+                cb(xhr.status);
+            }
         }
     };
     xhr.open("GET", url);
     xhr.send();
 }
 
+function sum(arr) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return arr.reduce(reducer)
+}
+
+
+
+function avg(arr, idx, range) {
+    return sum(arr.slice(idx - range, idx)) / range;
+}
+
+function sum(arr) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return arr.reduce(reducer)
+}
+
+function toFixed(n) {
+    return n.toFixed(2);
+}
+  
+
+if (typeof module !== "undefined") {
+    module.exports = { avg, sum, toFixed };
+}
--- scripts/sma.js
+++ scripts/sma.js
@@ -23,23 +23,6 @@ function calculateSma(number,arr, format,range=7) {
     changeIndecatorValue(number,res[res.length - 1]);
 }
 
-
-
-
-function avg(arr, idx, range) {
-    return sum(arr.slice(idx - range, idx)) / range;
-}
-  
-
-function sum(arr) {
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    return arr.reduce(reducer)
-}
-
-function toFixed(n) {
-    return n.toFixed(2);
-}
-  
  
   
 function smaclick(num,creptoName) {
